4ffe8f7804ec6f53ebc84f257fbc62855b3b376d
sivashanmugamo <shiva.omprakash@hotmail.com>
2020-10-15 15:03:42 -0400

    changes to controller_2

--- lab2/nodes/controller_2.py
+++ lab2/nodes/controller_2.py
@@ -8,13 +8,6 @@ from sensor_msgs.msg import LaserScan
 
 from controller_1 import polar_to_cartesian, cal_line_eq, cal_error, ransac
 
-# Initializing a publisher
-pub = rospy.Publisher(
-    name= '/cmd_vel',
-    data_class= Twist,
-    queue_size= 10
-)
-
 def subscriber_callback(msg):
     '''
     Callback function for the subscriber
@@ -24,10 +17,18 @@ def subscriber_callback(msg):
     '''
     # Creating a message for robot's mobility
     pub_msg = Twist()
+    print(msg)
 
 if __name__ == '__main__':
     # Initiatin a node
     rospy.init_node('test_node_2')
+
+    # Initializing a publisher
+    pub = rospy.Publisher(
+        name= '/cmd_vel',
+        data_class= Twist,
+        queue_size= 10
+    )
 
     # Initiating a subscriber
     sub = rospy.Subscriber(
