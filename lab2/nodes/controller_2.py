eb4a3dbf60154506058a4e039f3e5f739dbb6fb8
sivashanmugamo <shiva.omprakash@hotmail.com>
2020-10-17 01:02:48 -0400

    changes to controller_2.py

--- lab2/nodes/controller_2.py
+++ lab2/nodes/controller_2.py
@@ -12,16 +12,17 @@ from visualization_msgs.msg import Marker
 
 from controller_1 import polar_to_cartesian, cal_line_eq, cal_error, ransac
 
-rospy.init_node('test_node_2')
-
+# Inititalizing the initial & goal positions
 init_position = (-8.0, -2.0)
 goal_position = (4.5, 9.0)
 
+# Calculating the line connecting the initial & goal positions
 goal_line_param = cal_line_eq(
     point_1= init_position,
     point_2= goal_position
 )
 
+# Initiating a publisher
 pub = rospy.Publisher(
     name= '/cmd_vel',
     data_class= Twist,
@@ -29,15 +30,32 @@ pub = rospy.Publisher(
 )
 
 def sync_callback(laser_msg, odom_msg):
+    '''
+    Callback function for the subscribers
+
+    Input:
+        laser_msg -> LaserScan message
+        odom_msg -> Odometry message
+    Output:
+        *Still in the works*
+    '''
     bot_position = (odom_msg.pose.pose.position.x, odom_msg.pose.pose.position.y)
 
-if __name__ == '__main__':
-    # rospy.init_node('test_node_2')
+    if bot_position != init_position:
+        
 
+if __name__ == '__main__':
+    # Initiating a node
+    rospy.init_node('test_node_2')
+
+    # Initiating a subscriber for /base_scan
     laser_sub = message_filters.Subscriber('/base_scan', LaserScan)
+    # Initiating a subscriber for /odom
     odom_sub = message_filters.Subscriber('/odom', Odometry)
 
+    # To synchronize the messages from the 2 subscriber
     tst = message_filters.ApproximateTimeSynchronizer([laser_sub, odom_sub], 10, 0.2, True)
     tst.registerCallback(sync_callback)
 
+    # Runs the seesion infinitely
     rospy.spin()
