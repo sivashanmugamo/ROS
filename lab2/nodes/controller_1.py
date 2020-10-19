6d380a775a7c26617c8eb1e1403d4a3fe5a6cafd
sivashanmugamo <shiva.omprakash@hotmail.com>
2020-10-19 14:53:37 -0400

    cleaned and polished - Controller_1.py completed

--- lab2/nodes/controller_1.py
+++ lab2/nodes/controller_1.py
@@ -85,7 +85,7 @@ def cal_error(line_param, point):
     err= abs((a * x) + (b * y) + c) / math.sqrt((a*a) + (b*b))
 
     return err
-    
+
 def ransac(coordinates):
     '''
     Function to compute the RANSAC
@@ -93,72 +93,31 @@ def ransac(coordinates):
     Input:
         coordinates: Dictionary
     Output:
-        best_sample: Tuple of point tuples
+        best_lines: List of tuples of tuples - Eg: [((x1, y1), (x2, y2))]
     '''
-    # sampled_list = list()
-
-    # max_inlier_count= 0
-
-    # for each_iter in range(100):
-    #     inlier_count= 0
-
-    #     temp_dict= coordinates.copy()
-    #     current_sample= random.sample(range(1, len(coordinates)+1), 2)
-        
-    #     if (current_sample not in sampled_list) and (current_sample.reverse not in sampled_list):
-    #         sampled_list.append(current_sample)
-
-    #         pt_1= coordinates[current_sample[0]]
-    #         pt_2= coordinates[current_sample[1]]
-
-    #         (m, c)= cal_line_eq(
-    #             point_1= pt_1, 
-    #             point_2= pt_2
-    #         )
-
-    #         temp_dict.pop(current_sample[0])
-    #         temp_dict.pop(current_sample[1])
-
-    #         for key, each_point in temp_dict.items():
-    #             error= cal_error(
-    #                 line_param= (m, c),
-    #                 point= (each_point)
-    #             )
-
-    #             if error < 0.05:
-    #                 inlier_count += 1
-        
-    #     if inlier_count > max_inlier_count:
-    #         max_inlier_count = inlier_count
-    #         best_sample = (coordinates[current_sample[0]], coordinates[current_sample[1]])
-
-    # return best_sample
-
+    
     best_lines= list()
 
-    temp_coordinates= coordinates.copy()
-
-    while len(temp_coordinates) > 30:
+    while len(coordinates) > 30:
         max_inlier_count= 0
+
         sampled_list= list()
-        best_line_inliers= dict()
+        best_inlier_dict= dict()
 
         for each_iter in range(100):
-            inlier_count = 0
-            inlier_dict = dict()
-            temp_dict= temp_coordinates.copy()
+            inlier_count= 0
+            inlier_dict= dict()
 
-            current_sample= random.sample(temp_coordinates.keys(), 2)
+            temp_dict= coordinates.copy()
+
+            current_sample= random.sample(coordinates.keys(), 2)
 
             if (current_sample not in sampled_list) and (current_sample.reverse not in sampled_list):
                 sampled_list.append(current_sample)
 
-                pt_1= coordinates[current_sample[0]]
-                pt_2= coordinates[current_sample[1]]
-
                 (m, c)= cal_line_eq(
-                    point_1= pt_1,
-                    point_2= pt_2
+                    point_1= coordinates[current_sample[0]],
+                    point_2= coordinates[current_sample[1]]
                 )
 
                 temp_dict.pop(current_sample[0])
@@ -170,18 +129,18 @@ def ransac(coordinates):
                         point= each_point
                     )
 
-                    if error<0.05:
+                    if error < 0.05:
                         inlier_count += 1
                         inlier_dict[key]= each_point
 
             if inlier_count > max_inlier_count:
                 max_inlier_count= inlier_count
                 best_sample= (coordinates[current_sample[0]], coordinates[current_sample[1]])
-                best_line_inliers= inlier_dict.copy()
+                best_inlier_dict= inlier_dict.copy()
 
-        for each_key in best_line_inliers.keys():
-            temp_coordinates.pop(each_key)
-
+        for each_key in best_inlier_dict.keys():
+            coordinates.pop(each_key)
+        
         best_lines.append(best_sample)
 
     return best_lines
@@ -195,57 +154,45 @@ def subscriber_callback(msg):
     Output:
         pub_msg -> Publishing Marker message
     '''
+    
+    coordinates_dict= dict()
 
-    scan_range= msg.ranges
-    # scan_range= [i for i in msg.ranges if i!=3.0]
+    # Angles in message are in radians
+    min_angle= msg.angle_min
+    inc_angle= msg.angle_increment
 
-    flag= [0 if i==3.0 else 1 for i in scan_range]
+    i= 0
+    for each_range in msg.ranges:
+        if each_range != 3.0:
+            coordinates_dict[i+1] = polar_to_cartesian(
+                r= each_range,
+                theta= (min_angle + (i * inc_angle))
+            )
+        i += 1
+    
+    classifiers= ransac(
+        coordinates= coordinates_dict
+    )
 
-    # If the LaserScan detecs something then best line is found using RANSAC
-    if 1 in flag:
-        # Angles from the return "msg" will always be in radians
-        min_angle= msg.angle_min
-        inc_angle= 0
+    # Creating a message for rviz
+    pub_msg = Marker()
 
-        coordinates_dict= dict()
+    # Setting the message parameters
+    pub_msg.header.stamp = rospy.Time.now()
+    pub_msg.header.frame_id = '/base_link'
+    pub_msg.type = pub_msg.LINE_LIST
+    pub_msg.action = pub_msg.ADD
+    pub_msg.lifetime = rospy.Duration(10)
+    pub_msg.scale.x = 0.2
+    pub_msg.scale.y = 0.2
+    pub_msg.color.a = 1.0
+    pub_msg.color.r = 1.0
 
-        i= 1
-        for each_range in scan_range:
-            # if (each_range != 3.0):
-            (x, y)= polar_to_cartesian(r= each_range, theta= (min_angle + inc_angle))
-            inc_angle += msg.angle_increment
+    for each_classifier in classifiers:
+        pub_msg.points.append(Point(each_classifier[0][0], each_classifier[0][1], 0))
+        pub_msg.points.append(Point(each_classifier[1][0], each_classifier[1][1], 0))
 
-            coordinates_dict[i]= (x, y)
-
-            i += 1
-        
-        classifiers= ransac(coordinates_dict)
-
-        # Creating a message for rviz
-        pub_msg = Marker()
-
-        # Setting the message parameters
-        pub_msg.header.stamp = rospy.Time.now()
-        pub_msg.header.frame_id = '/base_link'
-        pub_msg.type = pub_msg.LINE_LIST
-        pub_msg.action = pub_msg.ADD
-        pub_msg.lifetime = rospy.Duration(10)
-        pub_msg.scale.x = 0.2
-        pub_msg.scale.y = 0.2
-        pub_msg.color.a = 1.0
-        pub_msg.color.r = 1.0
-
-        for each_classifier in classifiers:
-            # print(each_classifier)
-            pub_msg.points.append(Point(each_classifier[0][0], each_classifier[0][1], 0))
-            pub_msg.points.append(Point(each_classifier[1][0], each_classifier[1][1], 0))
-
-        pub.publish(pub_msg)
-
-    # Else an empty message is published
-    # else:
-    #     pub_msg = Marker()
-    #     pub.publish(pub_msg)
+    pub.publish(pub_msg)
 
 if __name__ == '__main__':
     # Initiating a node
