968a32f3cf294c52faed39698c0003de218a0839
sivashanmugamo <shiva.omprakash@hotmail.com>
2020-10-22 03:08:42 -0400

    controller_2 in the works

--- lab2/nodes/control_2.py
+++ lab2/nodes/control_2.py
@@ -15,7 +15,7 @@ from tf.transformations import euler_from_quaternion
 from controller_1 import polar_to_cartesian, cal_line_eq, cal_error, ransac
 
 # Initializing the robot's status
-bot_stat= 'GOALSEEK'
+bot_status= 'GOALSEEK'
 
 # z= 0.0 by default as the robot is in ground level
 bot_init= (-8.0, -2.0, 0.0)
@@ -28,33 +28,24 @@ pub= rospy.Publisher(
     queue_size= 10
 )
 
+# Creating a message object
+pub_msg= Twist()
+
 # Calculating the transit path
 goal_line_param= cal_line_eq(
     point_1= bot_init[:2],
     point_2= bot_goal[:2]
 )
 
-def cal_3d_dist(point_1, point_2):
-    '''
-    Calculates the distance between 2 points in 3D
-
-    Input:
-        point_1: Tuple of x, y, & z coordinates
-        point_2: Tuple of x, y, & z coordinates
-    Output:
-        dist: Float
-    '''
-
-    (x1, y1, z1)= point_1
-    (x2, y2, z2)= point_2
-
-    dist= math.sqrt(math.pow((x2-x1), 2)+math.pow((y2-y1), 2)+math.pow((z2-z1), 2))
-
-    return dist
-
 def cal_goal_angle(point_1, point_2):
     '''
     Calculates the angle to the goal point
+
+    Input:
+        point_1: Tuple of x, y, & z
+        point_2: Tuple of x, y, & z
+    Output:
+        angle: Float
     '''
 
     (x1, y1, z1)= point_1 
@@ -64,31 +55,81 @@ def cal_goal_angle(point_1, point_2):
 
     return angle
 
+def bot_orient(bot_angle, goal_angle):
+    rot_msg= Twist()
+    if (bot_angle - goal_angle)>0:
+        rot_msg.angular.z= (-1)*(bot_angle - goal_angle)
+        pub.publish(rot_msg)
+    else:
+        rot_msg.angular.z= abs(bot_angle - goal_angle)
+        pub.publish(rot_msg)
+
+
+def fw_possible(detect_data):
+    flag= [1 if i > 1.0 else 0 for i in detect_data[72: 288]]
+
+    if 0 in flag:
+        return False
+    else:
+        return True
+
+def bot_parallel(detect_data):
+    # print('Check if parallel')
+    print(len(detect_data))
+
+def bot_move(detect_data):
+    global bot_status
+    scan_data= detect_data.ranges
+    flag= [1 if i == 3.0 else 0 for i in scan_data]
+
+    mov_msg = Twist()
+    
+    if fw_possible(scan_data):
+        print('But robot can move')
+        mov_msg.linear.x= 0.5
+        pub.publish(mov_msg)
+    else:
+        print('Robot can\'t move')
+        bot_status= 'WALLFOLLOW'
+        bot_parallel(scan_data)
+
 def sync_callback(scan_msg, odom_msg):
+    print('------ NEW MESSAGE ------'+bot_status)
 
-    # bot_odom= odom_msg.pose.pose
-    # current_position= (bot_odom.position.x, bot_odom.position.y, bot_odom.position.z)
-    # current_orientation= (bot_odom.orientation.x, bot_odom.orientation.y, bot_odom.orientation.z, bot_odom.orientation.w)
+    bot_odom= odom_msg.pose.pose
+    current_position= (bot_odom.position.x, bot_odom.position.y, bot_odom.position.z)
+    current_orientation= (bot_odom.orientation.x, bot_odom.orientation.y, bot_odom.orientation.z, bot_odom.orientation.w)
 
-    # (roll, pitch, yaw) = euler_from_quaternion(list(current_orientation))
+    (roll, pitch, yaw) = euler_from_quaternion(list(current_orientation))
 
-    # (x1, y1, z1)= bot_init
-    # (x2, y2, z2)= bot_goal
+    # Calculated angle is measured in radians
+    angle= cal_goal_angle(
+        point_1= bot_init, 
+        point_2= bot_goal
+    )
 
-    # # Calculated angle is measured in radians
-    # angle= cal_goal_angle(
-    #     point_1= bot_init, 
-    #     point_2= bot_goal
-    # )
-
-    # dist= cal_3d_dist(
-    #     point_1= bot_init,
-    #     point_2= bot_goal
-    # )
-
-    # print(angle, dist)
+    if bot_init != bot_goal:
+        if bot_status == 'GOALSEEK':
+            if ((yaw+0.01 > angle) and (angle > yaw-0.01)) == False:
+                print('Turn')
+                bot_orient(
+                    bot_angle= yaw, 
+                    goal_angle= angle
+                )
+            else:
+                print('Don\'t turn')
+                bot_move(
+                    detect_data= scan_msg
+                )
+        elif bot_status == 'WALLFOLLOW':
+            bot_move(
+                detect_data= scan_msg
+            )
+        else:
+            raise Exception('Invalid bot status received')
 
 if __name__ == '__main__':
+    print(bot_status)
     # Initiating a node
     rospy.init_node('test_node_2')
 
